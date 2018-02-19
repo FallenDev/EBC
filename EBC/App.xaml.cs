256a3ee4a226f046c604f76db43e4428916c7296
Buck, Paul R <paul.buck86@gmail.com>
2018-02-19 11:43:20 -0500

    MetroApps added and application re-styled

--- EBC/App.xaml.cs
+++ EBC/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using MahApps.Metro;
 
 namespace EBC
 {
@@ -13,5 +14,14 @@ namespace EBC
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Tuple<AppTheme, Accent> appStyle = ThemeManager.DetectAppStyle(Application.Current);
+            ThemeManager.ChangeAppStyle(Application.Current,
+                ThemeManager.GetAccent("Steel"),
+                ThemeManager.GetAppTheme("BaseDark"));
+
+            base.OnStartup(e);
+        }
     }
 }
